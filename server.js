a4ab134eb4b62f2429b25a0427e184fdc0262825
p451 <antoni.duhov@gmail.com>
2024-12-05 01:07:35 +0200

    Update server.js

--- server.js
+++ server.js
@@ -867,14 +867,6 @@ app.get('/api/benefits', requireAuth, (req, res) => {
 
 
 
-// Static file serving for production
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
-
 // Server startup
 const PORT = process.env.PORT || 4000;  // Muutettu 3001 -> 8080
 app.listen(PORT, () => {
