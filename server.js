f6a2f462675b4389fe9159fc308bf4689c05db7f
p451 <antoni.duhov@gmail.com>
2024-12-05 21:28:15 +0200

    Update server.js

--- server.js
+++ server.js
@@ -39,6 +39,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Cookie, Set-Cookie');
   }
   
   if (req.method === 'OPTIONS') {
