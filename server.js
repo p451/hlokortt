e56cc779bf783eaa59f2baccc8a85e6c39be740e
p451 <antoni.duhovgmail.com>
2024-12-09 13:35:47 +0200

    wee

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ app.use((req, res, next) => {
 app.use((req, res, next) => {
   console.log('Session debug:', {
       sessionId: req.sessionID,
-      userId: req.session.userId,
+      userId: req.session ? req.session.userId : null,
       path: req.path,
       method: req.method,
       hasSession: !!req.session
