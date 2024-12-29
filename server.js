35bb83b75007a9acf4f69b4a4946b0e20bfc2e05
p451 <antoni.duhovgmail.com>
2024-12-29 22:53:42 +0200

    noen

--- server.js
+++ server.js
@@ -58,8 +58,8 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+    secure: true,
+    sameSite: 'none',
     maxAge: 24 * 60 * 60 * 1000, // 24h
     httpOnly: true
   },
